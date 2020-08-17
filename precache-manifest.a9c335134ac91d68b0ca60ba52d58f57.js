2f71c47e9d9805491154d45fc5c2319c4b8ad726
myshuker <myshuker@gmai.com>
2020-08-17 08:40:45 +0200

    Updates

---
+++ precache-manifest.a9c335134ac91d68b0ca60ba52d58f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e5890c2cf0d02a85fa02b048ba7348",
+    "url": "/Github-users-search/index.html"
+  },
+  {
+    "revision": "d01b05212808cbeaee33",
+    "url": "/Github-users-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/Github-users-search/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Github-users-search/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01b05212808cbeaee33",
+    "url": "/Github-users-search/static/js/main.531ecb60.chunk.js"
+  },
+  {
+    "revision": "9c306b793a145ccad5c3",
+    "url": "/Github-users-search/static/js/runtime-main.f895e37d.js"
+  }
+]);
